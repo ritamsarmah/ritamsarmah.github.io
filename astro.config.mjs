d531b24225ccc283f648940b53601970810b4a24
Ritam Sarmah <sarmah.ritam@gmail.com>
2024-05-24 16:37:34 -0600

    Use Astro

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://ritam.me",
+    redirects: {
+        "/projects/accelerate/faq-mac": "/projects/accelerate/faq#mac",
+        "/projects/accelerate/faq-ios": "/projects/accelerate/faq#ios",
+    },
+    integrations: [mdx(), tailwind(), svelte()],
+});
--- src/projects/cinesearch/index.md
+++
@@ -1,9 +0,0 @@
----
-title: Home
----
-
-![screenshot](../../images/cinesearch/screenshot.png)
-
-# Discover, search, and track movies.
-
-[![Download on App Store](../../images/appstore_white.svg)](https://apps.apple.com/tt/app/cinesearch/id1300312387)
